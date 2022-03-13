d3641e3e29a5bb1e91ab524d561e36405ef29088
Neidz <68917960+Neidz@users.noreply.github.com>
2022-03-13 18:14:46 +0100

    changed format of names of commands

--- src/routes/commands/newCommand.ts
+++ src/routes/commands/newCommand.ts
@@ -7,6 +7,7 @@ import { verifyToken } from "../authentication/verifyToken";
 router.post("/", verifyToken, async (req: Request, res: Response) => {
     const newCommand = new command(req.body);
     newCommand.username = req.user.username;
+    newCommand.name = `${req.user.username}_${newCommand.name}`;
     try {
         const savedCommand = await newCommand.save();
         res.status(200).json(savedCommand);
