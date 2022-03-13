6da7e65bb2d2cf5c1c47ee17ccc30b7287e2e6d0
Neidz <68917960+Neidz@users.noreply.github.com>
2022-03-13 10:44:09 +0100

    bug fix with data type on commandByName route

--- src/routes/commands/public/getCommandByName.ts
+++ src/routes/commands/public/getCommandByName.ts
@@ -6,7 +6,7 @@ import command from "../../../models/commandModel";
 router.get("/", async (req: Request, res: Response) => {
     const commandName = req.query.name;
     try {
-        const requestedCommand = await command.find({
+        const requestedCommand = await command.findOne({
             name: commandName,
         });
         res.status(200).json(requestedCommand);
